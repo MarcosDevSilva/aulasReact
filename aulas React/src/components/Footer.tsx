5f611ce22cc692c75ce42dad38a37f20e733f7ef
marcos <tassio50000@gmail.com>
2023-10-05 20:55:28 -0300

    segundo commit

--- aulas React/src/components/Footer.tsx
+++ aulas React/src/components/Footer.tsx
@@ -12,7 +12,7 @@ function Footer(){
   }
   return(
     <div> 
-    <h1>Primeiro teste</h1>
+    <h1>segundo teste</h1>
     <button onClick={ handleClick} type="button">Voltar</button>
     </div>
   )
